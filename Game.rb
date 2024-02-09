97d0c28bdbc67c5c14f00d921f2607d96179f740
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-09 10:39:55 -0700

    Add an instance variable to keep track of current player

--- Game.rb
+++ Game.rb
@@ -2,12 +2,15 @@ class Game
   def initialize
     @players = [Player.new("Player1"), Player.new("Player2")]
     @question = Question.new
+    @current_player_index = 0  # Start with the first player
   end
 
   def start_game
+    play_round
   end
 
   def play_round
+    if 
   end
 
   def next_turn
