d404932290f97da7f9d424a0651b4443d0d37599
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-09 10:50:05 -0700

    Implement functionality of play_round() method in Game class

--- Game.rb
+++ Game.rb
@@ -10,7 +10,14 @@ class Game
   end
 
   def play_round
-    if 
+    current_player = @players[@current_player_index]
+
+    if current_player.num_of_lives < 0
+      end_game
+    else
+      game_question = question.generate_question
+      puts "Player #{@current_player_index}: #{game_question}"
+      player_answer = current_player.answer_question(game_question)
   end
 
   def next_turn
