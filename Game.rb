59b315952efd9706db26fc7df6588b26698d088d
Hisban Shiraz <hisbanbardai@gmail.com>
2024-02-09 10:34:18 -0700

    Add necessary methods in Game class

--- Game.rb
+++ Game.rb
@@ -3,4 +3,16 @@ class Game
     @players = [Player.new("Player1"), Player.new("Player2")]
     @question = Question.new
   end
+
+  def start_game
+  end
+
+  def play_round
+  end
+
+  def next_turn
+  end
+
+  def end_game
+  end
 end
